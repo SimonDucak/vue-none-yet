88a15a894f81dede369c64c90bd2a30252f229a6
Simon Ducak <s.ducak@gmail.com>
2019-02-26 19:14:51 +0100

    Install axios

--- client/src/components/TheWebLines.vue
+++ client/src/components/TheWebLines.vue
@@ -1,25 +1,25 @@
 <template>
-  <div id="web-lines" class="flex count-children-8 block">
-    <div></div>
-    <div></div>
-    <div></div>
-    <div></div>
-    <div></div>
-    <div></div>
-    <div></div>
-    <div></div>
-  </div>
+  <!--<div id="web-lines" class="flex count-children-8 block">-->
+    <!--<div></div>-->
+    <!--<div></div>-->
+    <!--<div></div>-->
+    <!--<div></div>-->
+    <!--<div></div>-->
+    <!--<div></div>-->
+    <!--<div></div>-->
+    <!--<div></div>-->
+  <!--</div>-->
 </template>
 
 <style lang="scss" scoped>
-#web-lines {
-  position: fixed;
-  > * {
-    height: 100%;
-    border-right: 1px solid $border-light-color;
-    &:last-of-type {
-      border-right: none;
-    }
-  }
-}
+<!--#web-lines {-->
+  <!--position: fixed;-->
+  <!--&gt; * {-->
+    <!--height: 100%;-->
+    <!--border-right: 1px solid $border-light-color;-->
+    <!--&:last-of-type {-->
+      <!--border-right: none;-->
+    <!--}-->
+  <!--}-->
+<!--}-->
 </style>
